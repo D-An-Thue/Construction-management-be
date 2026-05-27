ac4688d46a5aae63ac01454442a3244ca0da7b59
baquydev <op3477662@gmail.com>
2026-05-27 15:09:01 +0700

    feat: update broadcasting configuration and improve user channel authorization logic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
+    )
+    ->withBroadcasting(
+        __DIR__.'/../routes/channels.php',
+        ['prefix' => 'api', 'middleware' => ['api', 'jwt']]
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
